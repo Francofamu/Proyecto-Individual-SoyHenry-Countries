f4f7167a49394e757f32cee426257c70e6fc3eef
Franco Famulari <famularifranco@gmail.com>
2023-11-14 12:42:01 -0300

    inicio PI

---
+++ cr-pi-countries-main/server/src/models/Activity.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    'Country', 
+    {
+        nombre: {
+           type: DataTypes.STRING,
+           allowNull: false
+        },
+        // descripcion: {
+        //    type: DataTypes.STRING,
+        //    allowNull: false,
+        // },
+        dificultad: {
+           type: DataTypes.INTEGER,
+           validate: {
+              min: 1,
+              max: 5
+           }
+        },
+        duracion: {
+           type: DataTypes.FLOAT,
+           validate: {
+              min: 1,
+              max: 48
+           }
+        },
+        temporada: {
+           type: DataTypes.ENUM('Verano', 'Otoño', 'Invierno', 'Primavera')
+        }
+     },
+     {
+        tableName: 'Activity',
+        timestamps: false
+     }
+  );
+};
